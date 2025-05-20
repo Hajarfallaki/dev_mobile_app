be53b37544805b134b711c9d2fff684d29d37e25
Hajarfallaki <hajarfallaki24@gmail.com>
2025-05-20 23:35:19 +0100

    Ajout des modifications sur map_page, main.dart, AndroidManifest, pubspec, etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-
+import 'Screens/map_page.dart';
 import 'Screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
       title: "Flutter Application",
-      home: HomePage(),
+//home: HomePage(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => HomePage(),
+
+        '/pagemap': (context) => MapPage(),
+      },
     );
   }
 }
